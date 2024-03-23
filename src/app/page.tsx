5f638bea9874db415f0f73f332ae3841a4696f1e
Robert-LC <robert_lc1@protonmail.com>
2024-03-23 06:23:55 -0400

    build: configure husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 import styles from './page.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -92,3 +92,5 @@ export default function Home() {
     </main>
   )
 }
+
+export default Home
