31583a72bae0b06ffe53c6255b7e3ff60a4d36a5
Robert-LC <robert_lc1@protonmail.com>
2024-03-28 23:04:23 -0400

    refactor: use tailwind instead of style comp for div

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -1,6 +1,5 @@
 'use client'
 import React from 'react'
-import styled from 'styled-components'
 
 import PianoKey from './PianoKey/PianoKey'
 import { NoteType } from './NoteType'
@@ -9,19 +8,14 @@ type Props = {
   notes: NoteType[]
 }
 
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
 const Piano: React.FC<Props> = ({ notes }) => (
-  <Wrapper>
+  <div className='flex items-center justify-center m-14'>
     <div>
       {notes.map((element: NoteType) => (
         <PianoKey key={element.note} note={element.note} color={element.color} />
       ))}
     </div>
-  </Wrapper>
+  </div>
 )
 
 export default Piano
