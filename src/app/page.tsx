85451dc3038581741a26a12ada949eb6c8370afd
Robert-LC <robert_lc1@protonmail.com>
2024-03-29 23:02:46 -0400

    feat: create game context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { GameProvider } from '@/contexts/GameContext'
+
 import Piano from '../components/Piano/Piano'
 import { notes } from '../components/Piano/NoteType'
 
 const Home = () => {
-  return <Piano notes={notes} />
+  return (
+    <GameProvider>
+      <Piano notes={notes} />
+    </GameProvider>
+  )
 }
 
 export default Home
---
+++ src/contexts/GameContext.tsx
@@ -0,0 +1,49 @@
+'use client'
+import React, { createContext, useReducer } from 'react'
+interface IProps {
+  children: React.ReactNode
+}
+
+interface GameState {
+  score: number
+  scale: string[]
+  note: string
+  mode: 'Major' | 'Minor' | 'Both'
+}
+
+interface GameAction {
+  type: 'SET_MODE' | 'INCREMENT_SCORE' | 'NEXT_NOTE' | 'NEXT_SCALE' | 'RESET_SCORE'
+  payload?: unknown | undefined
+}
+const initialState: GameState = {
+  score: 0,
+  scale: [],
+  note: '',
+  mode: 'Major'
+}
+const GameContext = createContext<
+  { state: GameState; dispatch: React.Dispatch<GameAction> } | undefined
+>(undefined)
+
+const gameReducer = (state: GameState, action: GameAction) => {
+  switch (action.type) {
+    case 'SET_MODE':
+      return { ...state, mode: action.payload as 'Major' | 'Minor' | 'Both' }
+    case 'INCREMENT_SCORE':
+      return { ...state, score: state.score + 1 }
+    case 'NEXT_NOTE':
+      return { ...state, note: action.payload as string }
+    case 'NEXT_SCALE':
+      return { ...state, scale: action.payload as string[] }
+    case 'RESET_SCORE':
+      return { ...state, score: 0 }
+    default:
+      return state
+  }
+}
+
+export const GameProvider = (props: IProps) => {
+  const [state, dispatch] = useReducer(gameReducer, initialState)
+
+  return <GameContext.Provider value={{ state, dispatch }}>{props.children}</GameContext.Provider>
+}
