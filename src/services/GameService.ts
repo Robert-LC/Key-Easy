666fcde9bb669a0d7ea15bcb4055058bad74aea3
Robert-LC <robert_lc1@protonmail.com>
2024-04-13 21:01:37 -0400

    refactor: remove redundant functions

--- src/services/GameService.ts
+++ src/services/GameService.ts
@@ -4,53 +4,28 @@ import { Scale } from '@/types/Scale'
 import { getRandomScale } from '@/utils/GameUtils'
 import { GameState } from '@/contexts/GameContext'
 
-/**
- * Retrieves the next note from the given array of notes.
- * This function implements a queue-based approach to track the current note and the remaining notes.
- *
- * @param notes - The array of notes.
- * @returns The next note, or `undefined` if there are no more notes.
- */
-export const getNextNote = (notes: Note[]): void | Note => {
-  // queue implentation, because we need FIFO and need to track current and next notes in one place
-  const nextNote = notes.shift()
-
-  if (!nextNote) {
-    return
-  }
-
-  return nextNote
-}
-
-/**
- * Retrieves the next scale from the given array of scales.
- * This function implements a stack-based approach to track the current scale and the remaining scales.
- *
- * @param scales - An array of scales.
- * @returns The next scale from the array, or `undefined` if there are no more scales.
- */
-export const getNextScale = (scales: Scale[]): void | Scale => {
-  const nextScale = scales.pop()
-
-  if (!nextScale) {
-    // endGame()
-  }
-
-  return nextScale
-}
-
 export const generateInitialGameState = (): GameState => {
   const scales = createScalesStack(ScaleMode.Major, 5)
-  const notes = createNotesQueue(scales[0])
-
+  const currentScale = scales.pop()
+  const notes = currentScale?.notes || []
+  const currentNote = notes.shift()
   return {
-    scales,
-    notes,
+    scales: scales,
+    notes: notes,
+    currentNote: currentNote,
+    currentScale: currentScale,
+    isGameInProgress: true,
     mode: ScaleMode.Major,
     score: 0,
     showNoteNames: false,
     triesLeft: 3
   }
+}
+
+export const isNoteCorrect = (clickedNote: Note, state: GameState): boolean => {
+  const isCorrect = state.currentNote?.nameNoOctave === clickedNote.nameNoOctave
+
+  return isCorrect
 }
 
 // Helper functions
@@ -63,10 +38,6 @@ const createScalesStack = (mode: ScaleMode, amount: number): Scale[] => {
   }
 
   return scales
-}
-
-const createNotesQueue = (scale: Scale): Note[] => {
-  return scale.notes
 }
 
 // const endGame() => {
