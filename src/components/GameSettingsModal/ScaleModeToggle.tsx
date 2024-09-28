1672f96d313a37ffe0de7103da568e0424c8435d
Robert-LC <robert_lc1@protonmail.com>
2024-09-28 13:55:41 -0400

    feat: add highlighting to selected scale mode

--- src/components/GameSettingsModal/ScaleModeToggle.tsx
+++ src/components/GameSettingsModal/ScaleModeToggle.tsx
@@ -8,21 +8,7 @@ type Props = {
 }
 
 const ScaleModeToggle = ({ onScaleChange, value }: Props) => {
-  const labels = {
-    left: {
-      title: 'Major',
-      value: ScaleMode.Major
-    },
-    center: {
-      title: 'Both',
-      value: ScaleMode.Both
-    },
-    right: {
-      title: 'Minor',
-      value: ScaleMode.Minor
-    }
-  }
-
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [scaleMode, setScaleMode] = useState<ScaleMode | null>(value)
 
   const handleOptionChange = (scaleMode: ScaleMode) => {
@@ -31,13 +17,30 @@ const ScaleModeToggle = ({ onScaleChange, value }: Props) => {
       onScaleChange(scaleMode)
       return scaleMode
     })
+
+    const btnIds = ['majorBtn', 'bothBtn', 'minorBtn']
+    btnIds.forEach((btnId: string) => {
+      document.getElementById(btnId)?.classList.remove('bg-purple-950', 'rounded-lg')
+    })
+
+    switch (scaleMode) {
+      case ScaleMode.Major:
+        document.getElementById('majorBtn')?.classList.add('bg-purple-950', 'rounded-lg')
+        break
+      case ScaleMode.Both:
+        document.getElementById('bothBtn')?.classList.add('bg-purple-950', 'rounded-lg')
+        break
+      case ScaleMode.Minor:
+        document.getElementById('minorBtn')?.classList.add('bg-purple-950', 'rounded-lg')
+        break
+    }
   }
 
   return (
     <div className='flex flex-col gap-4'>
       <h1>Scale Mode</h1>
       <div className='flex items-center justify-center space-x-2 border border-purple-500 rounded shadow-sm w-60 h-14'>
-        <div className={`text-purple-700`}>
+        <div id='majorBtn' className='text-purple-700 bg-purple-950 rounded-lg'>
           <input
             defaultChecked
             name='map-switch'
@@ -47,38 +50,36 @@ const ScaleModeToggle = ({ onScaleChange, value }: Props) => {
             className='hidden'
           />
           <label
-            className='left-label cursor-pointer'
+            className='cursor-pointer'
             htmlFor='left'
             onClick={() => handleOptionChange(ScaleMode.Major)}
           >
-            <h4>{labels.left.title}</h4>
+            <h4>Major</h4>
           </label>
         </div>
 
-        <div className='text-purple-700'>
+        <div id='bothBtn' className='text-purple-700'>
           <input name='map-switch' id='center' type='radio' value='center' className='hidden' />
           <label
-            className='center-label cursor-pointer'
+            className='cursor-pointer'
             htmlFor='center'
             onClick={() => handleOptionChange(ScaleMode.Both)}
           >
-            <h4>{labels.center.title}</h4>
+            <h4>Both</h4>
           </label>
         </div>
 
-        <div className='text-purple-700'>
+        <div id='minorBtn' className='text-purple-700'>
           <input name='map-switch' id='right' type='radio' value='right' className='hidden' />
           <label
-            className='right-label cursor-pointer'
+            className='cursor-pointer'
             htmlFor='right'
             onClick={() => handleOptionChange(ScaleMode.Minor)}
           >
-            <h4>{labels.right.title}</h4>
+            <h4>Minor</h4>
           </label>
         </div>
       </div>
-
-      <div>Selected Option: {scaleMode}</div>
     </div>
   )
 }
